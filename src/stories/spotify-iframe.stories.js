79b52aa2c4e69b69adf77316b1420a9bbd5a1efa
MohammedNagdy <mohammednagdy@hotmail.com>
2024-01-31 17:56:52 +0200

    Added the hook for that emits controller

--- src/stories/spotify-iframe.stories.js
+++ src/stories/spotify-iframe.stories.js
@@ -1,19 +1,37 @@
+import React from 'react';
 import SpotifyIframe from '../spotify-iframe';
 
-export default {
-  component: SpotifyIframe,
+
+export const Template = (args) => {
+	return <SpotifyIframe {...args} />;
 };
 
-/*
- * 
- */
-export const Primary = {
-  render: () => 
-        <SpotifyIframe 
-            id="1" 
-            spotifyId=""
-            type=""
-            height={200}
-            width={200} 
-        />,
+export const Default = Template.bind({});
+Default.storyName = 'Default';
+Default.args = {
+};
+
+export const DefaultSpotifyIframe = Template.bind({});
+DefaultSpotifyIframe.storyName = 'With Embed';
+DefaultSpotifyIframe.args = {
+	id: '1',
+  spotifyId:"5ChkMS8OtdzJeqyybCc9R5",
+  type:"track",
+  height:'100',
+  width:'100%',
+};
+
+export const AlbumSpotifyIframe = Template.bind({});
+AlbumSpotifyIframe.storyName = 'With Embed Album';
+AlbumSpotifyIframe.args = {
+	id: '2',
+  spotifyId:"1C2h7mLntPSeVYciMRTF4a",
+  type:"album",
+  height:'100',
+  width:'100%',
+};
+
+export default {
+	title: 'SpotifyIframe',
+	component: SpotifyIframe,
 };
